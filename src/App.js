49464ccdc9b9375cde5233555dd6a8ef1976fa9b
soneye omojuwon <calebomojuwon@gmail.com>
2020-11-05 23:06:55 +0100

    complete version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
+import wordTonumbers from 'words-to-numbers'
+import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from './style.js'
+const alankey = '5b9ada6fb5fc087d699746bac003cc962e956eca572e1d8b807a3e2338fdd0dc/stage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+    const [newsArticles, setNewArticles]= useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes= useStyles();
+useEffect(()=>{
+            alanBtn ({
+                key : alankey,
+                onCommand: ({ command, articles, number })=>{
+                    if (command === 'newHeadlines') {
+                       setNewArticles(articles)
+                       setActiveArticle(-1)
+                    }else if(command === 'highlight'){
+                        setActiveArticle((prevActiveArticle) => prevActiveArticle + 1 );
+                    } else if ( command === 'open'){
+                       const parsedNumber = number.length > 2 ? wordTonumbers(number, {fuzzy:true}) : number;
+                        const article = articles[parsedNumber -1 ];
+                        if (parsedNumber > 20 ) {
+                            alanBtn().playText('please try again')  
+                        } else if (article){
+                            window.open( article.url , '_blank');
+                        alanBtn().playText('opening')
+                        }
+                       
+                    }
+                }
+            })
+        }, []
+    )
+    return (
+        <div className={classes.logoontainer}> 
+        <p  className={classes.text}>ARTIFICIAL-INTELLIGENCE</p>
+        <p className={classes.text}>News Reader</p>
+       
+      
+        <NewsCards articles={newsArticles}  activeArticle={activeArticle}/>
+        </div>
+    );
 }
-
 export default App;
